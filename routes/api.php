86bb82f5274336095075de60cb75308019d99033
ab-dereje <abudereje09@gmail.com>
2023-08-05 19:16:57 -0400

    freelancer crud operation is done in the freelancercontroller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\FreelancerConroller;
 use App\Http\Controllers\PasswordResetController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\StockController;
+use App\Models\Freelancer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -33,3 +35,9 @@ Route::post('/stock/add', [StockController::class, 'store']);
 Route::get('/stock', [StockController::class, 'index']);
 Route::put('/stock/{id}', [StockController::class, 'update']);
 Route::delete('/stock/{id}', [StockController::class, 'destroy']);
+
+Route::post('/freelancer/add', [FreelancerConroller::class, 'store']);
+Route::get('/freelancer', [FreelancerConroller::class, 'index']);
+Route::put('/freelancer/{id}', [FreelancerConroller::class, 'update']);
+Route::delete('/freelancer/{id}', [FreelancerConroller::class, 'destroy']);
+Route::get('/freelancer/{id}',[FreelancerConroller::class, 'show']);
