084e235f42134756b4a081d43ca312c43a11eca8
ephyg <ephywaanofii@gmail.com>
2023-08-24 17:43:32 +0300

    add a middleware that returns an authenticated user information and move all routes that need authentication inside the auth:sanctum middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,7 @@ class UserController extends Controller
         $user = User::all();
         return $user;
     }
+    
     /**
      * Store a newly created resource in storage.
      */
--- routes/api.php
+++ routes/api.php
@@ -35,9 +35,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::middleware('auth:sanctum')->get('/user/verify', function () {
+    if (!auth()->user()) {
+        return response()->json([
+            'message' => "unauthorized user"
+        ], 401);
+    }
+    return auth()->user();
+});
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    
+
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/user/{id}', [UserController::class, 'show']);
     Route::post('/user', [UserController::class, 'store']);
@@ -45,57 +52,58 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::delete('/user/{id}', [UserController::class, 'destroy']);
     Route::post('/forgot', [PasswordResetController::class, 'sendResetCode']);
     Route::get('/user/email/{email}', [UserController::class, 'userFind']);
-    
-    
+
+
     // Not done
     Route::post('/stock/add', [StockController::class, 'store']);
     Route::get('/stock', [StockController::class, 'index']);
     Route::put('/stock/{id}', [StockController::class, 'update']);
     Route::delete('/stock/{id}', [StockController::class, 'destroy']);
-    
+
     Route::post('/freelancer/add', [FreelancerConroller::class, 'store']);
     Route::get('/freelancer', [FreelancerConroller::class, 'index']);
     Route::put('/freelancer/{id}', [FreelancerConroller::class, 'update']);
     Route::delete('/freelancer/{id}', [FreelancerConroller::class, 'destroy']);
-    
+
     Route::get('/basic_info', [Basic_infoController::class, 'index']);
     Route::put('/basic_info/{id}', [Basic_infoController::class, 'update']);
     Route::get('/freelancer/{id}', [FreelancerConroller::class, 'show']);
-    
+
     Route::get('/account_manager', [AccountManagerController::class, 'index']);
     Route::get('/employee', [EmployeeController::class, 'index']); //it also used from employ profile
     Route::get('/employee/{user_role}', [EmployeeController::class, 'staffList']);
     Route::get('/employee/{user_order}/{id}', [EmployeeController::class, 'employeeOrder']);
     // Route::get('/order/{id}', [EmployeeController::class, 'show']); // the controller is in the employeeController
-    
+
     Route::get('/agreement/{id}', [AgreementController::class, 'show']);
-    
+
     // done
     Route::get('/proforma', [ProformaController::class, 'index']);
     Route::post('/proforma/add', [ProformaController::class, 'store']);
     Route::get('/proforma/{id}', [ProformaController::class, 'show']);
     Route::put('/proforma/{id}', [ProformaController::class, 'update']);
     Route::delete('/proforma/{id}', [ProformaController::class, 'destroy']);
-    
+
     Route::get('/order/{id}', [OrderController::class, 'show']);
     Route::delete('/order/{id}', [OrderController::class, 'destroy']);
     Route::get('/order', [OrderController::class, 'index']);
-    
-    
+
+
     Route::post('/role/add', [RoleController::class, 'store']);
     Route::get('/role', [RoleController::class, 'index']);
     Route::put('/role/{id}', [RoleController::class, 'update']);
     Route::delete('/role/{id}', [RoleController::class, 'destroy']);
-    
-    
+
+
     // Route::get('/generate-report', [WeeklyReportController::class, 'generateReport']);
     Route::get('/report/{day}', [WeeklyReportController::class, 'show']);
     Route::get('/report', [WeeklyReportController::class, 'index']);
 });
+
+
 
 // done
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/forgot', [PasswordResetController::class, 'sendResetCode']);
 Route::post('/verifyCode', [PasswordResetController::class, 'checkCode']);
 Route::post('/changePassword', [PasswordResetController::class, 'changePassword']);
-
